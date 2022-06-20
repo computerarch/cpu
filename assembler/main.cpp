f1b4532a26ee4d2ccf315bbf6dff2d894dcd302a
Artaz <noobazi1212@gmail.com>
2022-06-20 19:45:37 +0430

    assembler fixed

--- assembler/main.cpp
+++ assembler/main.cpp
@@ -6,6 +6,59 @@ using namespace std;
 // R-type op rs rt rd
 // I-type op rs im rt
 // except beg => op rs rt im
+void createMap(unordered_map<string, char> *um)
+{
+    (*um)["0000"] = '0';
+    (*um)["0001"] = '1';
+    (*um)["0010"] = '2';
+    (*um)["0011"] = '3';
+    (*um)["0100"] = '4';
+    (*um)["0101"] = '5';
+    (*um)["0110"] = '6';
+    (*um)["0111"] = '7';
+    (*um)["1000"] = '8';
+    (*um)["1001"] = '9';
+    (*um)["1010"] = 'A';
+    (*um)["1011"] = 'B';
+    (*um)["1100"] = 'C';
+    (*um)["1101"] = 'D';
+    (*um)["1110"] = 'E';
+    (*um)["1111"] = 'F';
+}
+ QString convertBinToHex(string bin)
+{
+    int l = bin.size();
+    int t = bin.find_first_of('.');
+         int len_left = t != -1 ? t : l;
+    for (int i = 1; i <= (4 - len_left % 4) % 4; i++)
+        bin = '0' + bin;
+         if (t != -1)
+    {
+        int len_right = l - len_left - 1;
+        for (int i = 1; i <= (4 - len_right % 4) % 4; i++)
+            bin = bin + '0';
+    }
+    unordered_map<string, char> bin_hex_map;
+    createMap(&bin_hex_map);
+
+    int i = 0;
+    QString hex = "";
+
+    while (1)
+    {
+        hex += bin_hex_map[bin.substr(i, 4)];
+        i += 4;
+        if (i == bin.size())
+            break;
+        if (bin.at(i) == '.')
+        {
+            hex += '.';
+            i++;
+        }
+    }
+    return hex;
+}
+
 QString Convert_d_to_b(QString s)
 {
     int x = s.toInt();
@@ -30,7 +83,7 @@ QString Convert_d_to_b(QString s)
 QString Im_convert(QString s)
 {
     int x = s.toInt();
-    QString tmp = "0000000";
+    QString tmp = "00000000000";
     for (int i = tmp.length() - 1; i >= 0; i--)
     {
         if (x != 0)
@@ -70,63 +123,62 @@ QStringList IType(QString op,QString rs,QString rt,QString im)
 }
 int main(void)
 {
-    QFile ins("../assembler_input/str.txt");
+    QFile ins("str.txt");
     ins.open(QFile::Text | QFile::ReadOnly);
     QTextStream input(&ins);
-    QFile out("../assembler_output/out.txt");
-    out.open(QFile::Text | QFile::WriteOnly);
-    QTextStream output(&out);
+    QFile out2("out.txt");
+    QFile out16("out16.txt");
+    QTextStream output16 (&out16);
+    out2.open(QFile::Text | QFile::WriteOnly);
+    out16.open(QFile::Text | QFile::WriteOnly);
+    QTextStream output2(&out2);
     while (!input.atEnd())
     {
 
         QStringList result,data = input.readLine().split(' ');
         QString function = "",Reg = data[0],op = "00000";
+        QString str = "";
         if (Reg == "and")
         {
-            function = "00010";
+            function = "000000010";
             result = RType(op, data[1], data[2], data[3], function);
         }
         else if (Reg == "or")
         {
-            function = "00100";
+            function = "000000100";
             result = RType(op, data[1], data[2], data[3], function);
         }
         else if (Reg == "xor")
         {
-            function = "00110";
-            result = RType(op, data[1], data[2], data[3], function);
-        }
-        else if (Reg == "nor")
-        {
-            function = "01111";
+            function = "000000110";
             result = RType(op, data[1], data[2], data[3], function);
         }
         else if (Reg == "add")
         {
-            function = "01000";
+            function = "000001000";
             result = RType(op, data[1], data[2], data[3], function);
         }
         else if (Reg == "sub")
         {
-            function = "01010";
+            function = "000001010";
             result = RType(op, data[1], data[2], data[3], function);
         }
         else if (Reg == "mul")
         {
-            function = "01100";
+            function = "000001100";
             result = RType(op, data[1], data[2], data[3], function);
         }
         else if (Reg == "div")
         {
-            function = "01110";
+            function = "000001110";
             result = RType(op, data[1], data[2], data[3], function);
         }
-        else if (Reg == "sw")
+        else if (Reg == "lw")
         {
             op = "00011";
             result = IType(op,data[3],data[1],data[2]);
         }
-        else if (Reg == "lw")
+        else if (Reg == "sw")
         {
             op = "01011";
             result = IType(op,data[3],data[1],data[2]);
@@ -138,11 +190,15 @@ int main(void)
         }
         for (auto ii = result.begin() ; ii != result.end(); ii++)
         {
-            output << *ii;
+            output2 << *ii;
+            str += *ii;
         }
-        output << "\n";
+        output2 << "\n";
+        output16 << convertBinToHex(str.toStdString());
+        output16 << "\n";
     }
     ins.close();
-    out.close();
+    out2.close();
+    out16.close();
     return 0;
 }
